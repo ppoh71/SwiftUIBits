13684206aefceb2edb7bdc40f66f2cfc3e8308af
ppoh71 <ppoh71@gmail.com>
2021-01-08 12:35:13 +0000

    Upcoming UIBit SlideBars

---
+++ SwiftUIBits/SwiftUIBits/UIElements/SlideBars.swift
@@ -0,0 +1,21 @@
+//
+//  SlideBars.swift
+//  SwiftUIBits
+//
+//  Created by Peter Pohlmann on 08.01.21.
+//  Copyright © 2021 Peter Pohlmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct SlideBars: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SlideBars_Previews: PreviewProvider {
+    static var previews: some View {
+        SlideBars()
+    }
+}
